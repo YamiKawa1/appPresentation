24860d0e454032b6a9f771a6e25d373455202037
Jehan Veracierta <jehanzao@gmail.com>
2023-02-24 13:14:44 -0400

    sss

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -8,7 +8,6 @@ function Header() {
         <div className='name'>
             <h1><span>Launch Your App</span> with confidence And creativity</h1>
             <p className='details'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Veritatis nulla minima molestiae</p>
-            <a href="#" className='cv-btn'>Download</a>
         </div>
     </div>
   ) 
